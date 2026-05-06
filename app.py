5c738615fab4b510e536f7562a4b4d98aa784caa
manyashreeks <manyaksmanyaks35@gmail.com>
2026-05-06 13:45:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,17 @@ import qrcode
 from PIL import Image
 
 # page title
-st.title{"Qr code generator")
-data=st.text_input("enter ur1")
-if st.button("Generator QR"):
+st.title("Qr code generator")
+data=st.text_input("enter url")
+if st.button("Generate QR"):
     if data:
-         qr = qrcode.make(data)
-         qr.save("qr.png")
+      qr = qrcode.make(data)
+      qr.save("qr.png")
 
-         img = Image.open("qr.png")
-         st.image(img, caption="Generated QR Code")
-
-         with open("qr.png","rb") as f:
-            st.download_button("Download QR", f, file_name="qr.png")
-    else:
-         st.warning("please enter some text")
-           
+      img = Image.open("qr.png")
+      st.image(img, caption="Generated QR Code")
+      
+      with open("qr.png","rb") as f:
+          st.download_button("Download QR", f, file_name="qr.png")
+else:
+   st.warning("Please enter some text")
